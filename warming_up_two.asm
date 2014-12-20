ca0e5d98389641abc676eb5dd95b2874fcbc1dd0
MrJacek <jhojczak@wp.pl>
2014-12-20 21:30:32 +0100

    Dodanie wprawki odwrócenie napisu

--- warming_up_two.asm
+++ warming_up_two.asm
@@ -13,15 +13,14 @@ main:
 	
 	la $t0, buf # src - zapisanie do rejestru adresu buffor-a
 	move $t1,$t0 #dst - skopiowanie do drugiego rejestru adresu buffor-a
-
 nxtchr:	
 	lbu  $t2 ,($t0) # pobranie pierwszego znaku z buffora i zapisanie go do rejestru
 	addiu $t0,$t0,1 # zwiększenie rejestru o jeden (wskazanie na kolejny znak w stringu)
-	bltu $t2,' ', fin # sprawdzenie czy to koniec string-u SPACJA ma większą wartość niż koniec stringu 
+	bltu $t2,' ', fin # sprawdzenie czy to koniec string-u SPACJA ma większą wartość niż koniec stringu
 	beq $t2,' ',nxtchr # sprawdzenie czy znak to SPACJA jeśli nie..
-	sb $t2,($t1) # nad pisujemy w bufforze znak wskazywany przez 'wskaźnikiem' $t2 znakiem pod 'wskaźnikiem' $t1
+	sb $t2,($t1) # nad pisujemy w bufforze znak wskazywany przez 'wskaźnik' $t2 znakiem pod 'wskaźnikiem' $t1
 	addiu $t1, $t1, 1 # zwiększamy wskaźnik na nową tablice żeby wskazywał na nowy element
-	b nxtchr # pętla 
+	b nxtchr # pętla
 fin:
 	sb $zero, ($t1) # ustawienie zera na koniec nowego stringa
 	la $a0, buf # przygotowanie nowego stringa
