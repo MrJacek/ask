0f2ddb83816fd06dea32c71659cede81e8bee7b6
MrJacek <jhojczak@wp.pl>
2015-01-02 11:04:50 +0100

    Coś co prawie działa

--- main.c
+++ main.c
@@ -5,14 +5,10 @@ void removeNumber(const char* str);
 
 int main( int argc, const char* argv[] )
 {
-    printf( "\nHello World\n\n" );
     char str[100];
-
-    printf("Enter string: ");
-    gets(str);
-    
+    scanf("%s",str);
     removeNumber(str);
-    printf("inver string: %s\n",str);
+    printf("%s <=== result\n",str);
 
     return 0;
 }
--- removeNumber.s
+++ removeNumber.s
@@ -7,8 +7,6 @@ removeNumber:
     push    ebp ; zapamiętanie wskaźnika ramki procedury wołającej
     mov     ebp, esp ; ustanowienie własnego wskaźnika ramki
 ; procedura nie alokuje danych lokalnych na stosie
-    sub     esp,4 ; akumulator [esp+8]
-    sub     esp,4 ; akumulator tymczasowy [esp+4]
     sub     esp,8 ; adress początku najwiekszej liczby [esp]
 ; zwalnianie rejestrów
     push    ebx    
@@ -16,59 +14,55 @@ removeNumber:
     push    edi
     ; ecx asdress
 ; ciało procedury
-    mov     ecx,0
-    mov     edi, [ebp+8]    ; argument – wskaźnik na łańcuch
+    mov     eax, [ebp+8]    ; argument – wskaźnik na łańcuch
+    mov     esi,[ebp+8]
+
+
 lop1:
-    mov     dl, [edi]       ; kolejny bajt łańcucha
+    mov     dl, [eax]       ; kolejny bajt łańcucha
     cmp     dl,' '
     je      lop2init
     cmp     dl, ':'
-    jge     lop12
+    jge     lop13
     cmp     dl, '/'
-    ja      zapiszPozycje
-lop12:
-    ;je      zmienNajwiekszy
-lop121:
-    ;mov     dh,0
+    jbe     lop13
+
+    mov     [esp+8],eax
+    inc     eax
+    mov     dl,[eax]
+    cmp     dl,' '
+    cmp     dl,':'
+    cmp     dl,'/'
+    cmp     [esp+8],eax
+    je     lop2init
+
 lop13:
-    inc     edi
+    inc     eax
     jmp     lop1
 
-zapiszPozycje:
-   ; cmp     dh,1
-    mov     ecx,edi
-;    je      dodaj
-;    mov     dh,1
-;    mov     ebx,edi
-    jmp     lop13
+
+;==================================
+
 
 lop2init:
     mov     eax,[ebp+8]
-    mov     edi,eax
+    mov     ecx,eax
+
+
 lop2:
     mov     dl, [eax]       ; kolejny bajt łańcucha
     cmp     dl, ' '
     je      end
-    cmp     dl, ':'
-    jge     cos
-    cmp     dl, '/'
-    ja      cos2 
-;    cmp    dl,'1'
-;    je      lop21
+    
+    cmp     eax,[esp+8]
+    je      lop21
+
 cos:
-    mov     [edi],dl
-    inc     edi
+    mov     [ecx],dl
+    inc     ecx
 lop21:
     inc     eax
     jmp     lop2
-
-cos2:
-    cmp     ecx,eax
-    je      lop21
-    jmp     cos
-
-
-
 
 
 ; epilog
