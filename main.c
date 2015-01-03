5b1ff0e971f2293169ee9653465435c2b1e60770
MrJacek <jhojczak@wp.pl>
2015-01-03 01:02:59 +0100

    Prawie działa

--- main.c
+++ main.c
@@ -6,6 +6,14 @@ void removeNumber(const char* str);
 int main( int argc, const char* argv[] )
 {
     char str[100];
+/**    str[4]='a';
+    str[5]='1';
+    str[6]='a';
+    str[7]='a';
+    str[8]='a';
+    str[9]='a';
+    str[10]=0;
+**/
     scanf("%s",str);
     removeNumber(str);
     printf("%s <=== result\n",str);
--- removeNumber.s
+++ removeNumber.s
@@ -7,7 +7,10 @@ removeNumber:
     push    ebp ; zapamiętanie wskaźnika ramki procedury wołającej
     mov     ebp, esp ; ustanowienie własnego wskaźnika ramki
 ; procedura nie alokuje danych lokalnych na stosie
-    sub     esp,8 ; adress początku najwiekszej liczby [esp]
+    sub     esp,4
+    sub     esp,8
+    sub     esp,8 ; akumulator
+    sub     esp,1
 ; zwalnianie rejestrów
     push    ebx    
     push    esi
@@ -15,58 +18,94 @@ removeNumber:
     ; ecx asdress
 ; ciało procedury
     mov     eax, [ebp+8]    ; argument – wskaźnik na łańcuch
-    mov     esi,[ebp+8]
-
-
+    mov     edx,0
+    mov     [ebp-4],dx
+    mov     [ebp-12],edx
+    mov     edx,0
+    mov     ecx,0
+    mov     si,0
+    mov     [ebp-13],dh
 lop1:
     mov     dl, [eax]       ; kolejny bajt łańcucha
-    cmp     dl,' '
-    je      lop2init
+    test     dl,dl
+    jz      lop2init
     cmp     dl, ':'
-    jge     lop13
+    jge     lop12
     cmp     dl, '/'
-    jbe     lop13
+    jbe     lop12
+;    mov     ebx,eax
 
-    mov     [esp+8],eax
-    inc     eax
-    mov     dl,[eax]
-    cmp     dl,' '
-    cmp     dl,':'
-    cmp     dl,'/'
-    cmp     [esp+8],eax
-    je     lop2init
+    jmp     zapisz
+lop12:
+    mov     dh,0
+    mov     [ebp-13],dh
+    cmp     cx,[ebp-4]
+    jb     lop13
+    mov     [ebp-4],cx 
+    mov     [ebp-12],ebx
+    mov     cx,0
 
 lop13:
     inc     eax
     jmp     lop1
 
-
 ;==================================
+zapisz:
+    mov     dh,1
+    cmp     [ebp-13],dh ;Sprawdzamy czy jest kontnuacja liczby
+    je      dodaj ; jesli tak to przechodzimy do dodawani jej wartosci
+    mov     ebx,eax ; jak nie to zapisujem adres poczatkowy tej liczby
+    mov     [ebp-13],dh ; ustawiamy flage
+dodaj:
+    sub     dl,48
+    mov     [ebp-20],eax
+    mov     eax,0
+    ;mov     al,10
+    ;mul     cx
+    add     al,dl
+    add     cx,ax
+    mov     eax,[ebp-20]
+    jmp     lop13
+    
 
-
+;=================================
 lop2init:
+    mov     ebx,[ebp-12]
     mov     eax,[ebp+8]
     mov     ecx,eax
-
+    mov     dh,0
 
 lop2:
     mov     dl, [eax]       ; kolejny bajt łańcucha
-    cmp     dl, ' '
-    je      end
-    
-    cmp     eax,[esp+8]
-    je      lop21
+    test    dl, dl
+    jz      end
 
-cos:
+    cmp     eax,ebx
+    je      flaga
+    cmp     dl, ':'
+    jge     lop20
+    cmp     dl, '/'
+    jbe     lop20
+    cmp     dh,1
+    je      lop22
+lop20:
+    mov     dh,0
+lop21:
     mov     [ecx],dl
     inc     ecx
-lop21:
+lop22:
     inc     eax
     jmp     lop2
+
+flaga:
+    mov     dh,1
+    jmp     lop22
 
 
 ; epilog
 end:
+    mov     dl,0
+    mov     [ecx],dl
 ;   przywrucenie rejestrow
     pop     edi
     pop     esi
